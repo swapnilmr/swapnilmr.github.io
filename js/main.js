b574684d22757b1707380bd200c066e43d03f53c
Swapnil Rane <swapnilmr@outlook.com>
2023-08-02 20:41:53 +0530

    Product Designer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAYABhDynyFu_vDOVN2kF8dLTYYyArfw3Y",
+//     authDomain: "my-portfolio-7de9f.firebaseapp.com",
+//     projectId: "my-portfolio-7de9f",
+//     storageBucket: "my-portfolio-7de9f.appspot.com",
+//     messagingSenderId: "476583123335",
+//     appId: "1:476583123335:web:919309f8ebb207a262db22",
+//     measurementId: "G-2E0XSZW3SM"
+// };
+// // Initialize Firebase
+// const app = firebase.initializeApp(firebaseConfig);
+// const db = firebase.firestore(app);
+
 $(function () {
     $(document).scroll(function () {
         var $nav = $("#mainNav");
@@ -18,47 +36,10 @@ $(function () {
 
     $('[data-toggle="tooltip"]').tooltip();
 
-    var graphicAnime = anime({
-        targets: '#graphic path',
-        strokeDashoffset: [anime.setDashoffset, 0],
-        easing: 'easeInCubic',
-        duration: 3000,
-        autoplay: false
-    });
-
-    var uiAnime = anime({
-        targets: '#UI path',
-        strokeDashoffset: [anime.setDashoffset, 0],
-        easing: 'easeInCubic',
-        duration: 3000,
-        autoplay: false
-    });
-
-    var developAnime = anime({
-        targets: '#development path',
-        strokeDashoffset: [anime.setDashoffset, 0],
-        easing: 'easeInCubic',
-        duration: 3000,
-        autoplay: false
-    });
-
-    var waypoint1 = $('#graphic').waypoint(function () {
-        graphicAnime.play();
-    }, {
-        offset: '50%'
-    });
-
-    var waypoint2 = $('#UI').waypoint(function () {
-        uiAnime.play();
-    }, {
-        offset: '50%'
-    });
-
-    var waypoint3 = $('#development').waypoint(function () {
-        developAnime.play();
-    }, {
-        offset: '50%'
-    });
-
 
 });
+
+
+
+
+
